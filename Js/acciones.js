1ff4af7aaba62eefe170b757aa2fdf1329471d66
MateoGranda <granda.digitales@gmail.com>
2022-11-13 18:34:48 -0500

    dr
    dr

---
+++ Js/acciones.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("nav-item-links");
+const menu_container = document.getElementById("nav-container");
+const menu_dest = document.getElementById("menu-dest");
+const menu_act = document.getElementById("menu-act");
+const reglas_normas = document.getElementById("reglas-normas-hover-continer");
+
+
+function abrir_reglas_normas() {
+    reglas_normas.style.display = "block";
+
+  }
+function cerrar_reglas_normas()
+{
+    reglas_normas.style.display = "none";
+}
